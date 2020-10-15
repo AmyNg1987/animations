7516f13e2ad1af63c90a8a0dc6b388fe30a1c640
AmyNg1987 <ansb1987@gmail.com>
2020-10-16 02:07:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,6 @@
     var instances = M.Carousel.init(elems, options);
   });
 
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, options);
-  });
-
 var elem = document.querySelector('.collapsible.expandable');
 var instance = M.Collapsible.init(elem, {
   accordion: false
