fb09036ef107f5a7a7a9135706cb431cf803d577
AmyNg1987 <ansb1987@gmail.com>
2020-10-16 01:58:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,8 @@
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, options);
   });
+
+var elem = document.querySelector('.collapsible.expandable');
+var instance = M.Collapsible.init(elem, {
+  accordion: false
+});
