2a9125d78630a7e65e0e622e2f6abe205876e2cc
AmyNg1987 <ansb1987@gmail.com>
2020-10-16 01:38:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, options);
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
